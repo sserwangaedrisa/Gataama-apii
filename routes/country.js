ae222ff6e36dbf435ad467339e96c535f8497c23
Naol Ketema <naolketema55@gmail.com>
2024-09-24 22:56:03 +0300

    done

--- routes/country.js
+++ routes/country.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const countryController = require("../controllers/country");
 const verifyToken = require("../middleware/auth");
-const { isAdmin } = require("../middleware/role");
+const { isAdmin, isCountryAdmin } = require("../middleware/role");
 const upload = require("../middleware/image-upload");
 
 
 router.post("/", verifyToken, isAdmin,   upload.single("image"), countryController.createCountry);
 router.get("/", countryController.getAllCountries);
 router.get("/:id", countryController.getCountryById);
-router.put("/:id", verifyToken, isAdmin,   upload.single("image"), countryController.updateCountry);
+router.put("/:id", verifyToken, isCountryAdmin,   upload.single("image"), countryController.updateCountry);
 router.delete("/remove-admin/:id", verifyToken, isAdmin, countryController.removeCountryAdmin);
 router.delete("/:id", verifyToken, isAdmin, countryController.deleteCountry);
 

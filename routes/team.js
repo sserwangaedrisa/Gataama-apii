d97239b76c94ff9ee9ca63d4dc1fcc92aea99457
Naol Ketema <naolketema55@gmail.com>
2024-09-28 00:39:20 +0300

    refactor: Update team routes for main and country-specific endpoints

--- routes/team.js
+++ routes/team.js
@@ -5,18 +5,18 @@ const verifyToken = require("../middleware/auth");
 const upload = require("../middleware/image-upload");
 
 const { isCountryAdmin, isAdmin } = require("../middleware/role");
-
-router.post('/:countryId',verifyToken, isCountryAdmin, upload.single("profilePicture"), teamsController.addTeamMember);
 router.post('/main',verifyToken, isAdmin, upload.single("profilePicture"), teamsController.addTeamMemberForMain);
 
+router.post('/:countryId',verifyToken, isCountryAdmin, upload.single("profilePicture"), teamsController.addTeamMember);
 
 
-router.get('/:countryId', teamsController.getTeamMembersByCountry);
 router.get('/main', teamsController.getTeamsMain);
 
+router.get('/:countryId', teamsController.getTeamMembersByCountry);
+
+router.put('/main/:teamMemberId', verifyToken, isAdmin, upload.single("profilePicture"), teamsController.updateTeamMemberForMain);
 
 router.put('/:countryId/:teamMemberId', verifyToken, isCountryAdmin, upload.single("profilePicture"), teamsController.updateTeamMember);
-router.put('/main/:teamMemberId', verifyToken, isAdmin, upload.single("profilePicture"), teamsController.updateTeamMemberForMain);
 
 
 router.delete('/:countryId/:teamMemberId', verifyToken, isCountryAdmin,teamsController.deleteTeamMember);
